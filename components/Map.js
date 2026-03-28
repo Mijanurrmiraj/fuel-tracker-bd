3d516b845b465a38fbe8b93ea70ba05a0750fe31
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-03-28 19:46:08 +0600

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -2,19 +2,14 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-export default function Map({ pumps }) {
+export default function Map() {
   return (
-    <MapContainer center={[23.6850, 90.3563]} zoom={7} style={{ height: '60vh' }}>
+    <MapContainer center={[23.6850, 90.3563]} zoom={7} style={{ height: "80vh" }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      {pumps.map((pump, i) => (
-        <Marker key={i} position={[pump.lat, pump.lng]}>
-          <Popup>
-            <b>{pump.name}</b><br/>
-            সময়: {pump.time}
-          </Popup>
-        </Marker>
-      ))}
+      <Marker position={[23.6850, 90.3563]}>
+        <Popup>Fuel Available</Popup>
+      </Marker>
     </MapContainer>
   );
 }
