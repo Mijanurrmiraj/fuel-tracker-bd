fe7dd181ca56001605014e5021006f65d35f5d7a
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-03-28 21:57:18 +0600

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,36 +1,73 @@
 "use client";
-import { db } from "./firebase";
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-// 📍 Map click detect
-function LocationMarker({ onMapClick }) {
-  useMapEvents({
-    click(e) {
-      onMapClick(e.latlng);
-    },
-  });
-  return null;
-}
+import { useEffect, useState } from "react";
+import { db } from "./firebase";
 
-export default function Map({ pumps = [], onMapClick }) {
+import {
+  collection,
+  addDoc,
+  getDocs
+} from "firebase/firestore";
+
+export default function Map() {
+  const [pumps, setPumps] = useState([]);
+
+  // 🔥 Load data from Firebase
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "pumps"));
+      const data = querySnapshot.docs.map(doc => doc.data());
+      setPumps(data);
+    };
+
+    fetchData();
+  }, []);
+
+  // ➕ Add Pump
+  const addPump = async () => {
+    const name = prompt("Pump name?");
+    const lat = prompt("Latitude?");
+    const lng = prompt("Longitude?");
+    const fuel = prompt("Fuel type?");
+    const time = prompt("Minutes available?");
+
+    const newPump = {
+      name,
+      lat: parseFloat(lat),
+      lng: parseFloat(lng),
+      fuel,
+      time
+    };
+
+    // 🔥 Save to Firebase
+    await addDoc(collection(db, "pumps"), newPump);
+
+    // update UI instantly
+    setPumps([...pumps, newPump]);
+  };
+
   return (
-    <div style={{ height: "60vh" }}>
+    <div>
+      <button onClick={addPump}>+ Add Pump</button>
+
       <MapContainer
         center={[23.685, 90.3563]}
         zoom={7}
-        style={{ height: "100%", width: "100%" }}
+        style={{ height: "90vh" }}
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-        <LocationMarker onMapClick={onMapClick} />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
         {pumps.map((pump, i) => (
           <Marker key={i} position={[pump.lat, pump.lng]}>
             <Popup>
-              <b>{pump.name}</b> <br />
-              ⛽ {pump.fuel} <br />
-              ⏳ {pump.minutes} min
+              <b>{pump.name}</b><br/>
+              Fuel: {pump.fuel} <br/>
+              Time: {pump.time} min
             </Popup>
           </Marker>
         ))}
