5ade30443f85deb9c165642dca5d0a0786813287
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-03-28 22:31:11 +0600

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,77 +1,38 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 
-import { useEffect, useState } from "react";
-import { db } from "./firebase";
+function AddMarker({ onAdd }) {
+  useMapEvents({
+    click(e) {
+      onAdd(e.latlng.lat, e.latlng.lng);
+    },
+  });
+  return null;
+}
 
-import {
-  collection,
-  addDoc,
-  getDocs
-} from "firebase/firestore";
-
-export default function Map() {
-  const [pumps, setPumps] = useState([]);
-
-  // 🔥 Load data from Firebase
-  useEffect(() => {
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, "pumps"));
-      const data = querySnapshot.docs.map(doc => doc.data());
-      setPumps(data);
-    };
-
-    fetchData();
-  }, []);
-
-  // ➕ Add Pump
-  const addPump = async () => {
-    const name = prompt("Pump name?");
-    const lat = prompt("Latitude?");
-    const lng = prompt("Longitude?");
-    const fuel = prompt("Fuel type?");
-    const time = prompt("Minutes available?");
-
-    const newPump = {
-      name,
-      lat: parseFloat(lat),
-      lng: parseFloat(lng),
-      fuel,
-      time
-    };
-
-    // 🔥 Save to Firebase
-    await addDoc(collection(db, "pumps"), newPump);
-
-    // update UI instantly
-    setPumps([...pumps, newPump]);
-  };
-
+export default function Map({ pumps, onAddPump }) {
   return (
-    <div>
-      <button onClick={addPump}>+ Add Pump</button>
+    <MapContainer
+      center={[23.685, 90.3563]}
+      zoom={7}
+      style={{ height: "80vh", width: "100%" }}
+    >
+      <TileLayer
+        attribution="&copy; OpenStreetMap"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-      <MapContainer
-        center={[23.685, 90.3563]}
-        zoom={7}
-        style={{ height: "90vh" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <AddMarker onAdd={onAddPump} />
 
-        {pumps.map((pump, i) => (
-          <Marker key={i} position={[pump.lat, pump.lng]}>
-            <Popup>
-              <b>{pump.name}</b><br/>
-              Fuel: {pump.fuel} <br/>
-              Time: {pump.time} min
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    </div>
+      {pumps.map((pump, i) => (
+        <Marker key={i} position={[pump.lat, pump.lng]}>
+          <Popup>
+            ⛽ {pump.fuelType} <br />
+            ⏳ {pump.minutes} minutes
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
