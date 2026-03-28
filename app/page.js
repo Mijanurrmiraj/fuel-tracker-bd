b910493749aa160342201ec2ce1f64c4bbf0e17e
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-03-28 19:42:19 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,37 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const Map = dynamic(() => import('../components/Map'), {
-  ssr: false,
-});
+import { useEffect, useState } from 'react';
+import Map from '../components/Map';
+import Form from '../components/Form';
+import { db } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 export default function Home() {
-  return <Map />;
+  const [pumps, setPumps] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "pumps"));
+      const data = querySnapshot.docs.map(doc => doc.data());
+      setPumps(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Form />
+      <Map pumps={pumps} />
+
+      <div style={{ padding: 10 }}>
+        <h3>📍 Fuel Available</h3>
+        {pumps.map((p, i) => (
+          <div key={i}>
+            <b>{p.name}</b> - {p.time}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
