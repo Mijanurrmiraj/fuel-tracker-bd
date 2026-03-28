504811eeba14f6b9caa9df1157aa8a7412c6a6ba
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-03-28 21:05:08 +0600

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,28 +1,9 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useEffect, useState } from "react";
-import { db } from "../lib/firebase";
-import { collection, onSnapshot } from "firebase/firestore";
 import "leaflet/dist/leaflet.css";
 
-export default function Map() {
-  const [pumps, setPumps] = useState([]);
-
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, "pumps"), (snapshot) => {
-      setPumps(snapshot.docs.map(doc => doc.data()));
-    });
-
-    return () => unsub();
-  }, []);
-
-  const getTimeLeft = (endTime) => {
-    const diff = endTime - Date.now();
-    if (diff <= 0) return "❌ শেষ";
-    return Math.floor(diff / 60000) + " min";
-  };
-
+export default function Map({ pumps }) {
   return (
     <MapContainer center={[23.685, 90.3563]} zoom={7} style={{ height: "80vh" }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
@@ -30,9 +11,9 @@ export default function Map() {
       {pumps.map((pump, i) => (
         <Marker key={i} position={[pump.lat, pump.lng]}>
           <Popup>
-            <b>{pump.name}</b><br />
-            Fuel: {pump.fuel}<br />
-            ⏳ {getTimeLeft(pump.endTime)}
+            <b>{pump.name}</b> <br />
+            Fuel: {pump.fuel} <br />
+            Time left: {pump.minutes} min
           </Popup>
         </Marker>
       ))}
