0e22b84a72936af0af35c6f4c6ca3cb67cb9b01e
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-03-28 20:39:00 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,9 +5,8 @@ import dynamic from "next/dynamic";
 const Map = dynamic(() => import("../components/Map"), { ssr: false });
 
 export default function Home() {
-  return (
-    <div>
-      <h2>⛽ Fuel Map BD</h2>
+  return <h1>Fuel Map BD</h1>;
+}
       <Map />
     </div>
   );
