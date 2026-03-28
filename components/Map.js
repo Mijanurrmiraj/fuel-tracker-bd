c7237a948c9e32a5adeaef1bab771679f96c2b92
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-03-28 21:10:23 +0600

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,16 +1,24 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
 
 export default function Map({ pumps = [] }) {
+  const [Leaflet, setLeaflet] = useState(null);
+
+  useEffect(() => {
+    import("leaflet").then((L) => {
+      setLeaflet(L);
+    });
+  }, []);
+
+  if (!Leaflet) return <p>Loading map...</p>;
+
+  const { MapContainer, TileLayer, Marker, Popup } =
+    require("react-leaflet");
+
   return (
-    <div style={{ height: "80vh", width: "100%" }}>
-      <MapContainer
-        center={[23.685, 90.3563]}
-        zoom={7}
-        style={{ height: "100%", width: "100%" }}
-      >
+    <div style={{ height: "80vh" }}>
+      <MapContainer center={[23.685, 90.3563]} zoom={7} style={{ height: "100%" }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
         {pumps.map((pump, i) => (
