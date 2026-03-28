f4253393ee29fe08e93ccd567af32c0b713b16e6
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-03-28 21:53:00 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,9 +5,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyDeJVH1Zpj2-P-zenFPI0Z00DLrGqZJ8co",
   authDomain: "fuel-tracker-bd-fcd3e.firebaseapp.com",
   projectId: "fuel-tracker-bd-fcd3e",
-  storageBucket: "fuel-tracker-bd-fcd3e.appspot.com",
+  storageBucket: "fuel-tracker-bd-fcd3e.firebasestorage.app",
   messagingSenderId: "773182498776",
   appId: "1:773182498776:web:885520c00d1b36d0fef4ad",
+  measurementId: "G-XK2EB939MV"
 };
 
 const app = initializeApp(firebaseConfig);
